7161636076b5edaf33a3002a71f65ce4152fa75d
Isaac1th <Isaacadis1th@gmail.com>
2023-03-12 15:03:22 +0200

    add header & home components, style the header components

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: `./product-header.component.html`,
+})
+export class ProductsHeaderComponent implements OnInit {
+  sort = 'desc';
+  itemsShowCount = 12;
+
+  constructor() {}
+  ngOnInit(): void {}
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+}
